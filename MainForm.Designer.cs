db1c7d021cb4c2f8e839f259bd896f7ce3173605
rohitchan666 <120973690+rohitchan666@users.noreply.github.com>
2022-12-19 00:21:43 +0530

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,189 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: 
+ * Date: 01-Dec-22
+ * Time: 1:01 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace WallChangeV2
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.label4 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.timer2 = new System.Windows.Forms.Timer(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(376, 40);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(88, 32);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Add Images";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(376, 80);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(88, 32);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "Clear List";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(48, 200);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(280, 168);
+			this.pictureBox1.TabIndex = 4;
+			this.pictureBox1.TabStop = false;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(376, 152);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(88, 32);
+			this.button3.TabIndex = 5;
+			this.button3.Text = "Apply";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(16, 8);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(81, 17);
+			this.label1.TabIndex = 6;
+			this.label1.Text = "Wallpapers:";
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.ForeColor = System.Drawing.Color.Red;
+			this.label4.Location = new System.Drawing.Point(16, 416);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(40, 15);
+			this.label4.TabIndex = 11;
+			this.label4.Text = "label4";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(240, 384);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(64, 23);
+			this.comboBox1.TabIndex = 12;
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 15;
+			this.listBox1.Location = new System.Drawing.Point(16, 32);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(336, 154);
+			this.listBox1.TabIndex = 14;
+			this.listBox1.SelectedIndexChanged += new System.EventHandler(this.ListBox1SelectedIndexChanged);
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.Location = new System.Drawing.Point(16, 384);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(224, 24);
+			this.checkBox1.TabIndex = 15;
+			this.checkBox1.Text = "Automatically Change After Every :";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1CheckedChanged);
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.Timer1Tick);
+			// 
+			// timer2
+			// 
+			this.timer2.Tick += new System.EventHandler(this.Timer2Tick);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(484, 449);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Name = "MainForm";
+			this.Text = "WallChange v2.0";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Timer timer2;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,201 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: 
+ * Date: 01-Dec-22
+ * Time: 1:01 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace WallChangeV2
+{
+	
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		
+		int intTimerInterval = 0;
+		int intSeconds = 0;
+		
+		//Code for calling Windows functions, bcoz .NET doesnt have inbuilt function of changing wallpaper
+		[DllImport("user32.dll", SetLastError = true)]
+	    [return: MarshalAs(UnmanagedType.Bool)]
+	    static extern bool SystemParametersInfo(uint uiAction, uint uiParam, String pvParam, uint fWinIni);	
+		private const uint SPI_SETDESKWALLPAPER = 0x14;
+	    private const uint SPIF_UPDATEINIFILE = 0x1;
+	    private const uint SPIF_SENDWININICHANGE = 0x2;
+	    
+		public static void SetWallpaper(String strpath)
+		{
+		    SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, strpath, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+		}
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			//Set Initial values
+			openFileDialog1.Filter = "Images (*.jpg)|*.jpg";
+			openFileDialog1.Multiselect = true;
+			openFileDialog1.CheckFileExists = true;
+			openFileDialog1.CheckPathExists = true;
+			openFileDialog1.ReadOnlyChecked = true;
+			openFileDialog1.FileName = "";
+			
+			comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+			comboBox1.Items.Add("2 Min");
+			comboBox1.Items.Add("5 Min");
+			comboBox1.Items.Add("10 Min");
+			comboBox1.Items.Add("15 Min");
+			comboBox1.Items.Add("30 Min");
+			comboBox1.Items.Add("60 Min");
+			comboBox1.SelectedIndex = 0;
+			
+			label4.Text = "";
+	
+			pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+			pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+			
+			timer1.Interval = 1000; //1 sec
+			timer2.Interval = 60000; //1 min
+			
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+	
+			string[] strFileName;
+			
+			if (openFileDialog1.ShowDialog() == DialogResult.OK) 
+			{
+
+				strFileName = openFileDialog1.FileNames;
+				//MessageBox.Show(Convert.ToString(strFileName.Length));
+				
+				for (int i = 0; i < strFileName.Length; i++) 
+				{
+					listBox1.Items.Add(strFileName[i]);
+				}
+
+			}
+
+		}
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			listBox1.Items.Clear();
+			timer1.Enabled = false;
+		}
+		
+		void Button3Click(object sender, EventArgs e)
+		{
+			if (listBox1.Items.Count == 0) {
+				//Emptry listbox, Exit
+				return;
+			}
+			
+			SetWallpaper(pictureBox1.ImageLocation);
+			
+		}
+		
+		void ListBox1SelectedIndexChanged(object sender, EventArgs e)
+		{
+			pictureBox1.ImageLocation = listBox1.SelectedItem.ToString();
+		}
+		
+		void CheckBox1CheckedChanged(object sender, EventArgs e)
+		{
+
+			switch (checkBox1.Checked) {
+				case true:
+					if (listBox1.Items.Count == 0) 
+					{
+						MessageBox.Show("No Images file in listbox", Application.ProductName, MessageBoxButtons.OK);
+						return;
+					}
+					
+					switch (comboBox1.SelectedIndex) 
+					{
+						case 0: //2 min 
+							intTimerInterval = 2;
+							break;
+						case 1: //5 min
+							intTimerInterval = 5;
+							break;
+						case 2: //10 min
+							intTimerInterval = 10;
+							break;
+						case 3: // 15 min
+							intTimerInterval = 15;
+							break;
+						case 4: //30 min
+							intTimerInterval = 30;
+							break;
+						case 5: //60 Min
+							intTimerInterval = 60;
+							break;
+					}
+				
+			
+					//1 second = 1000 miliseconds
+					//1 min = 60000 miliseconds
+					timer2.Interval = intTimerInterval * 60000;
+					timer1.Enabled = true;
+					timer2.Enabled = true;
+					
+					//multiple min to 60 seconds to get seconds then -1 to get negetive value.
+					intSeconds = (intTimerInterval * 60);
+					
+					break;
+				case false:
+					timer1.Enabled = false;
+					timer2.Enabled = false;
+					label4.Text = "";
+					break;
+			}
+			
+		}
+		
+		void Timer1Tick(object sender, EventArgs e)
+		{	
+			//timer 1 for counting seconds.
+			intSeconds = intSeconds - 1;
+			label4.Text = "Next Change in : " + intSeconds.ToString() + " Seconds...";
+		}
+		
+		void Timer2Tick(object sender, EventArgs e)
+		{
+			Random rnd = new Random();
+			// creates a number between 0 and listbox number of images
+			int myrandom = rnd.Next(0, listBox1.Items.Count - 1);
+			
+			listBox1.SetSelected(myrandom, true);
+			
+			SetWallpaper(pictureBox1.ImageLocation);
+			
+			//multiple min to 60 seconds to get seconds then -1 to get negetive value.
+			intSeconds = (intTimerInterval * 60);
+			
+		}
+		
+		
+	}
+}
